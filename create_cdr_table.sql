5da43e81d5db326bcb31b40394c18d3ea4029485
Shafique Jamal <shafique.jamal@gmail.com>
2013-12-21 17:17:52 -0500

    added extensions.conf. Changed data model to have one table for white and blacklist. Using generic sql - putting sql in dialplan.

--- create_cdr_table.sql
+++ create_cdr_table.sql
@@ -1,6 +1,9 @@
 CREATE TABLE cdr (
-      calldate datetime NOT NULL default '0000-00-00 00:00:00',
       clid varchar(80) NOT NULL default '',
+      callerid_all varchar(80) NOT NULL default '',
+      callerid_name varchar(80) NOT NULL default '',
+      callerid_num varchar(80) NOT NULL default '',
+      callerid_dnid varchar(80) NOT NULL default '',
       src varchar(80) NOT NULL default '',
       dst varchar(80) NOT NULL default '',
       dcontext varchar(80) NOT NULL default '',
@@ -8,6 +11,9 @@ CREATE TABLE cdr (
       dstchannel varchar(80) NOT NULL default '',
       lastapp varchar(80) NOT NULL default '',
       lastdata varchar(80) NOT NULL default '',
+      start datetime NOT NULL default '0000-00-00 00:00:00',
+      answer datetime NOT NULL default '0000-00-00 00:00:00',
+      end datetime NOT NULL default '0000-00-00 00:00:00',
       duration int(11) NOT NULL default '0',
       billsec int(11) NOT NULL default '0',
       disposition varchar(45) NOT NULL default '',
@@ -18,4 +24,15 @@ CREATE TABLE cdr (
       peeraccount varchar(20) NOT NULL default '',
       linkedid varchar(32) NOT NULL default '',
       sequence int(11) NOT NULL default '0'
+);
+
+CREATE TABLE numberlist (
+      callerid_all varchar(80) NOT NULL default '',
+      callerid_num varchar(80) NOT NULL default '',
+      callerid_name varchar(80) NOT NULL default '',
+      list_type int NOT NULL default 1
+);
+
+CREATE TABLE extensions_passthrough (
+      extension varchar(80) NOT NULL
 );
