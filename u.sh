2425c5fbb142ac843ceca9b46deef6e6f1f085a5
Shafique Jamal <shafique.jamal@gmail.com>
2013-12-22 00:23:58 -0500

    updated extensions.conf

--- u.sh
+++ u.sh
@@ -1,6 +1,7 @@
+scp extensions.conf root@192.168.0.33:/etc/asterisk/
+ssh root@192.168.0.33 'asterisk -rx "dialplan reload"'
 git status
 git add .
 git commit -m "updated extensions.conf"
 git push origin master
 git status
-
