e4cabc47918c6a38815dec3237a8fe690e3ab185
Shafique Jamal <shafique.jamal@gmail.com>
2013-12-23 01:34:11 -0500

    updated extensions.conf

--- u.sh
+++ u.sh
@@ -1,5 +1,5 @@
-# scp extensions.conf root@192.168.0.33:/etc/asterisk/
-# ssh root@192.168.0.33 'asterisk -rx "dialplan reload"'
+scp extensions.conf root@192.168.0.33:/etc/asterisk/
+ssh root@192.168.0.33 'asterisk -rx "dialplan reload"'
 scp extensions.conf shafique2@192.168.0.102:/etc/asterisk/
 ssh shafique2@192.168.0.102 'sudo asterisk -rx "dialplan reload"'
 scp extensions.conf shafique@192.168.0.101:/etc/asterisk/
