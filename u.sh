cf52878a02ff976765492ef6eb739d66671c60ea
Shafique Jamal <shafique.jamal@gmail.com>
2013-12-22 19:14:54 -0500

    updated extensions.conf

--- u.sh
+++ u.sh
@@ -1,9 +1,9 @@
 # scp extensions.conf root@192.168.0.33:/etc/asterisk/
 # ssh root@192.168.0.33 'asterisk -rx "dialplan reload"'
 scp extensions.conf shafique2@192.168.0.102:/etc/asterisk/
-ssh shafique2@192.168.0.102 'asterisk -rx "dialplan reload"'
+ssh shafique2@192.168.0.102 'sudo asterisk -rx "dialplan reload"'
 scp extensions.conf shafique@192.168.0.101:/etc/asterisk/
-ssh shafique@192.168.0.101 'asterisk -rx "dialplan reload"'
+ssh shafique@192.168.0.101 'sudo asterisk -rx "dialplan reload"'
 git status
 git add .
 git commit -m "updated extensions.conf"
