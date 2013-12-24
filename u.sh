5bb2474418b14d32ee6014796a45f7d618eebf08
Shafique Jamal <shafique.jamal@gmail.com>
2013-12-23 21:18:48 -0500

    added cr to last line of res_odbc.conf

--- u.sh
+++ u.sh
@@ -5,8 +5,8 @@ ssh shafique2@192.168.0.102 'sudo asterisk -rx "dialplan reload"'
 scp extensions.conf shafique@192.168.0.101:~/
 ssh shafique@192.168.0.101 'sudo mv ~/extensions.conf /etc/asterisk/extensions.conf'
 ssh shafique@192.168.0.101 'sudo asterisk -rx "dialplan reload"'
-git status
-git add .
-git commit -m "updated extensions.conf"
-git push origin master
-git status
+#git status
+#git add .
+#git commit -m "updated extensions.conf"
+#git push origin master
+#git status
